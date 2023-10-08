f88934c4a927fc1a50d792945229a5ed1f045b98
NVA8 <95127908+NVA8@users.noreply.github.com>
2023-10-08 12:12:03 +0300

    Initial Commit

---
+++ weather_app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather_app
+//
+//  Created by Валерий Никитин on 08.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
